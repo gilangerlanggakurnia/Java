aafc79e1caa5a6afb356b5c8831f2e11c6699f53
Harpan20 <regytaharpan20102001@gmail.com>
2019-11-20 15:22:17 +0700

    Java

---
+++ Latihan 6/src/latihan/pkg6/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg6;
+
+/**
+ *
+ * @author ASUS
+ * NAMA                 : Gilang Erlangga 
+ * KELAS                : TI -1B
+ * NIM                  : A2.1900072
+ */
+public class Mamalia {
+    // Variabel jumlahKambing dideklarasika sebagai statik
+    public static int jumlahKambing;
+    
+}
